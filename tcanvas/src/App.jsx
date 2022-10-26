0031057175db42a54662e30849724713593f2532
alextitonis <alexandrostitonis@gmail.com>
2022-10-26 16:22:27 +0300

    updated react version and added a new html version

--- tcanvas/src/App.jsx
+++ tcanvas/src/App.jsx
@@ -1,39 +1,10 @@
-import { useState } from "react";
 import "./App.css";
-import Perlin from "../../utils/perlinNoise";
-import { cubicNoiseConfig, cubicNoiseSample2 } from "../../utils/cubicNoise";
+import SimpleGenerator from "./SimpleGenerator";
 
 function App() {
-  useState(() => {
-    const noise = new Perlin();
-    const perlinRes = noise.noise(1, 4);
-    console.log("perlin res", perlinRes);
-
-    const cubicNoise = cubicNoiseSample2(cubicNoiseConfig(5, 0, 0), 1, 4);
-    console.log("cubic res", cubicNoise);
-
-    const canvas = document.querySelector("canvas");
-    if (!canvas) {
-      return;
-    }
-
-    console.log(canvas);
-    const ctx = canvas.getContext("2d");
-
-    const newImgData = ctx.createImageData(canvas.width, canvas.height);
-    for (let i = 0; i < newImgData.data.length; i += 4) {
-      newImgData.data[i] = 0;
-      newImgData.data[i + 1] = 0;
-      newImgData.data[i + 2] = 0;
-      newImgData.data[i + 3] = 255;
-    }
-    ctx.putImageData(newImgData, 0, 0);
-  }, []);
-
   return (
     <div className="App">
-      <h2>Map</h2>
-      <canvas width={640} height={640}></canvas>
+      <SimpleGenerator />
     </div>
   );
 }
